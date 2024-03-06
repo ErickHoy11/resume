d4b3cd4d71c95ab7f94d09bfc2c1503226586463
ErickHoy11 <124842751+ErickHoy11@users.noreply.github.com>
2024-03-05 21:21:22 -0500

    cleaned up code. Added css for nicer formatting Changed footer a content Added js functionality to hire button on homepage

---
+++ index.js
@@ -0,0 +1,11 @@
+const heroBannerRight = document.querySelector("#hero_banner_right");
+
+function replace_hire_button(){
+    heroBannerRight.innerHTML = `
+    <p>Unfortunately, I can't program how to hire myself at your company</p>
+    <p>Instead, here is my contact information:</p>
+    <h1>Erick Hoy</h1>
+    <p>Phone: 570-377-0932</p>
+    <p>Email: erickhoy11@gmail.com</p>
+    `;
+}
